fc155ac2fc8845ab25334aa902f2ef1341e58a04
Gonzalo Balay Losada <gbalay@denodo.com>
2026-02-11 16:29:04 +0100

    disbale button

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import { useAuth } from "../context/AuthContext.jsx";
 export default function HomePage() {
     const { navigateTo } = useRouter()
     const { isLoggedIn } = useAuth()
-    
+    //TODO get prediction status to show button only when predictions are open or user has already made a prediction
     
     return (
         <main>
@@ -15,7 +15,10 @@ export default function HomePage() {
                     <h3>THE WORLD STAGE AWAITS</h3>
                     <h4>Predict winners, earn points and won prizes!</h4>
                     <span></span>
-                    <button className={isLoggedIn ? styles.predictionBtn : `${styles.predictionBtn} ${styles.disabled}`} onClick={() => navigateTo('/prediction')}>Start Predicting</button>
+                    <button className={isLoggedIn ? styles.predictionBtn : `${styles.predictionBtn} ${styles.btnDisabled}`} 
+                        onClick={() => navigateTo('/prediction')}>
+                            Start Predicting
+                        </button>
                 </div>
             </section>
         </main>
