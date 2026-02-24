497b7c52312e6ef8288f7a26ce5a69f35c4c95a3
Gonzalo Balay Losada <gbalay@denodo.com>
2026-02-24 11:26:35 +0100

    add not found PAge

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Home from './pages/Home.jsx'
 import { Leaderboard } from './pages/Leaderboard.jsx'
+import NotFoundPage from './pages/NotFoundPage.jsx'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/leaderBoard" element={<Leaderboard />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </>
   )
